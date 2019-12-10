647087b719102f4dd3b3fde89c7931cf674e1c40
Jingyuan Wang <jingyuanwang@u.northwestern.edu>
2019-12-09 20:24:20 -0600

    Update entry_estimation.py
    add OLS

--- 450-1-Final/entry/entry_estimation.py
+++ 450-1-Final/entry/entry_estimation.py
@@ -17,6 +17,8 @@ import pandas as pd
 import os,sys,inspect
 import scipy.stats as stats
 import scipy.integrate as integrate
+import econtools 
+import econtools.metrics as mt
 import itertools as it
 import copy
 import importlib
@@ -30,7 +32,7 @@ np.random.seed(seed=13344)
 # Class
 # ---------------------------------------------------------------------------------
 
-class Entry_MLE_Estimation:
+class Entry_Estimation:
     '''Estimateion: MLE+MPEC '''
 
     # get the main dataframe (and get the set of variables)
@@ -49,6 +51,7 @@ class Entry_MLE_Estimation:
         # prepare for estimation
         self.prepare_for_MLE()
         self.extract_true_value()
+        self.prepare_for_twostepOLS()
 
     # I. basic functions -------------------------------------------------------------------
     def prepare_for_MLE(self):
@@ -59,6 +62,17 @@ class Entry_MLE_Estimation:
         self.data_N_m = self.entry_sample.sample[2,:]
         self.data_X_m = self.entry_sample.sample[3,:]
         
+        return
+
+    def prepare_for_twostepOLS(self):
+        '''Preparation: save observables in dataframe '''
+        
+        self.sample_df = pd.DataFrame(data = self.entry_sample.sample.T, columns = ['N_A', 'N_B', 'N_m', 'X_m'])  
+
+        self.extract_true_value()
+        self.sample_df['P_0'] = self.true_prob[0,:]
+        self.sample_df['P_A'] = self.true_prob[1,:]
+        self.sample_df['P_B'] = self.true_prob[2,:]
 
         return
 
@@ -71,7 +85,7 @@ class Entry_MLE_Estimation:
         return 
 
 
-    # II. Objective and contraints ---------------------------------------------------------
+    # II. MLE ---------------------------------------------------------
     def obj_loglikelihood(self, theta_input):
 
         # data
@@ -92,6 +106,62 @@ class Entry_MLE_Estimation:
         obj = sum(loglikelihood)
 
         return obj
+
+    # III. Two-step ------------------------------------------------------
+    def OLS(self, true_prob=False):
+
+        df = self._OLS_gen_var()
+
+        res_stage1 = self._OLS_first_stage(df, true_prob)
+        res_stage2 = self._OLS_second_stage(df, true_prob)
+
+        # extract parameters
+        se = np.insert(res_stage1.se.values, [2,4], res_stage2.se.values)
+        beta = np.insert(res_stage1.beta.values, [2,4], res_stage2.beta.values)
+        p = np.insert(res_stage1.pt.values, [2,4], res_stage2.pt.values)
+        res = pd.DataFrame(np.array([beta,se,p]).T, 
+                     index = ['beta0', 'beta1', 'beta2', 'gamma1', 'gamma2', 'gamma2-gamma1'], 
+                     columns = ['beta', 'se', 'p>t'])
+
+        return res
+
+    def _OLS_gen_var(self):
+
+        # 1. get data
+        df = self.sample_df.copy()
+
+        # 2. generate variables
+        # dependent:
+        df['log_PA_PB'] = np.log(df['N_A']/df['N_B'])
+        df['N_0'] = df['N_m'] - df['N_A'] - df['N_B']
+        df['log_PB_P0'] = np.log(df['N_B']/df['N_0'])
+
+        df['log_PA_PB_true'] = np.log(df['P_A']/df['P_B'])  
+        df['log_PB_P0_true'] = np.log(df['P_B']/df['P_0'])      
+
+        # independent var:
+        df['const'] = 1
+        df['NA_NB_diff'] = df['N_A'] - df['N_B']
+
+        return df
+
+    def _OLS_first_stage(self, df, true_prob = False):
+
+        if true_prob:
+            res = mt.reg(df,'log_PB_P0_true', ['const', 'X_m','N_B', 'N_A'] )
+        else:
+            res = mt.reg(df,'log_PB_P0', ['const', 'X_m','N_B', 'N_A'] )
+
+        return res
+
+    def _OLS_second_stage(self, df, true_prob = False):
+
+        if true_prob:
+            res = mt.reg(df,'log_PA_PB_true', ['const','NA_NB_diff'] )
+        else:
+            res = mt.reg(df,'log_PA_PB', ['const','NA_NB_diff'] )
+
+        return res
 
 
 
